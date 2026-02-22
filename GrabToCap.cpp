a059e7481c91795d0f088d7f8fc343b66352b355
Zehra <Zehra@users.noreply.github.com>
2026-02-21 22:06:03 -0500

    minor cleanupf of GrabToCap.cpp

--- GrabToCap.cpp
+++ GrabToCap.cpp
@@ -2,13 +2,6 @@
 
 #include "bzfsAPI.h"
 
-// Custom Utility functions
-void resetPlayer(int playerID) {
-    bz_resetFlag(playerID);
-    bz_killPlayer(playerID, true, BZ_SERVER);
-    bz_incrementPlayerLosses(playerID, -1);
-}
-
 bz_eTeamType flagToTeamValue(const char* flagType) {
     if (strcmp("R*", flagType) == 0) {
         return eRedTeam;
@@ -27,18 +20,10 @@ class GrabToCap : public bz_Plugin
 {
 public:
   virtual const char* Name () {return "GrabToCap";}
-  virtual void Init(const char* config);
-  virtual void Event(bz_EventData *eventData);
-};
-
-BZ_PLUGIN(GrabToCap)
-
-void GrabToCap::Init( const char* /*commandLine*/ ) {
-  Register(bz_eFlagGrabbedEvent);
-}
-
-void GrabToCap::Event ( bz_EventData *eventData )
-{
+  virtual void Init( const char* commandLine ) {
+    Register(bz_eFlagGrabbedEvent);
+  }
+  virtual void Event ( bz_EventData *eventData ) {
     if (eventData->eventType != bz_eFlagGrabbedEvent)
       return;
 
@@ -49,17 +34,25 @@ void GrabToCap::Event ( bz_EventData *eventData )
 
     // Proceeding, if we have a team flag.
     if (flagTeam != eNoTeam) {
-        bz_eTeamType playerTeam = bz_getPlayerTeam(grabData->playerID);
-        // If we have an actual team, we may be ready to score.
-        if ((playerTeam == eRedTeam) || (playerTeam == eGreenTeam) || (playerTeam == eBlueTeam) || (playerTeam == ePurpleTeam)) {
-            // Checking if teams are not the same, if they are not, we trigger a score change at this point, as well as anything else specified.
-            if (flagTeam != playerTeam) {
-                // View custom functions for details of code.
-                bz_triggerFlagCapture(playerHoldingFlag, playerTeam, flagTeam);
-                resetPlayer(playerHoldingFlag);
-                //bz_incrementPlayerWins(playerHoldingFlag, bz_getTeamCount(flagTeam));
-            }
+      bz_eTeamType playerTeam = bz_getPlayerTeam(grabData->playerID);
+      // If we have an actual team, we may be ready to score.
+      if ((playerTeam == eRedTeam) || (playerTeam == eGreenTeam) || (playerTeam == eBlueTeam) || (playerTeam == ePurpleTeam)) {
+        // Checking if teams are not the same, if they are not, we trigger a score change at this point, as well as anything else specified.
+        if (flagTeam != playerTeam) {
+          // View custom functions for details of code.
+          bz_triggerFlagCapture(playerHoldingFlag, playerTeam, flagTeam);
+          bz_resetFlag(playerID);
+          bz_killPlayer(playerID, true, BZ_SERVER);
+          bz_incrementPlayerLosses(playerID, -1);
         }
+      }
     }
     // End of event trigger
-}
+  }
+  virtual void Cleanup(void) {
+    Flush();
+  }
+};
+
+BZ_PLUGIN(GrabToCap)
+
